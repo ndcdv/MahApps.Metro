a1aba0df02cbaf40e4e94c2f0eaf5a33a3c60da8
ndcdv <david.vlaming@dematic.com>
2017-02-06 17:23:39 +1100

    New Geometry Menu Item for HamburgerMenu

---
+++ src/MahApps.Metro/MahApps.Metro.Shared/Controls/HamburgerMenu/MenuItems/HamburgerMenuGeometryItem.cs
@@ -0,0 +1,31 @@
+﻿namespace MahApps.Metro.Controls
+{
+	using System.Windows;
+	using System.Windows.Media;
+	/// <summary>
+	/// The HamburgerMenuItem provides a glyph based implementation for HamburgerMenu entries.
+	/// </summary>
+	public class HamburgerMenuGeometryItem : HamburgerMenuItem
+    {
+		/// <summary>
+		/// Identifies the <see cref="GeometryData"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty GeometryDataProperty = DependencyProperty.Register(nameof(Geometry), typeof(Geometry), typeof(HamburgerMenuItem), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets gets of sets a value that specifies the Geometry to use for a Path
+        /// </summary>
+        public Geometry GeometryData
+		{
+            get
+            {
+                return (Geometry)GetValue(GeometryDataProperty);
+            }
+
+            set
+            {
+                SetValue(GeometryDataProperty, value);
+            }
+        }
+    }
+}
